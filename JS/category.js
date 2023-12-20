839223064f0540706fa49cb8b3faeceece5db3ca
Ruslan <ruslanri@code.edu.az>
2023-12-21 01:41:23 +0400

    Updatecategory

--- JS/category.js
+++ JS/category.js
@@ -9,3 +9,14 @@ menus.forEach((menu) => {
     dropdown.classList.toggle("disable");
   });
 });
+
+const open = document.querySelector(".product-sidebar-filter__button");
+const close = document.querySelector("#closeSidebar");
+const menu = document.querySelector(".left-site-menu");
+
+open.addEventListener("click", () => {
+  menu.classList.add("visible");
+});
+close.addEventListener("click", () => {
+  menu.classList.remove("visible");
+});
